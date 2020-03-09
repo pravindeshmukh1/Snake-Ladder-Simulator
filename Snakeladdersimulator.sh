dc051983a1e4b9f3d09a9040d7074c29cbcbb7e4
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-09 13:03:47 +0530

    ADDED: check the position ladder snake and noPlay

--- Snakeladdersimulator.sh
+++ Snakeladdersimulator.sh
@@ -3,10 +3,28 @@
 echo "welcome Snake & Ladder Simulator"
 
 #constant
-position=0
+START_POSITION=0
+
+NO_PLAY=0
+LADDER=1
+SNAKE=2
+
+playerPosition=0
 
 
 function getNumber() {
 	rno=$((RANDOM%6+1))
+	echo $rno
 }
 getNumber
+
+function checkOption () {
+		choice=$((RANDOM%3))
+	case $choice in
+		$NO_PLAY) playerPosition=$(($playerPosition+0)) ;;
+		$LADDER)	playerPosition=$(( $playerPosition+$rno )) ;;
+		$SNAKE) playerPosition=$(( $playerPositon-$rno )) ;;
+	esac
+		echo $playerPosition
+}
+checkOption
