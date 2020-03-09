8914215f3e44367f9d1daa38456d91cdeeb467e8
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-09 16:20:56 +0530

    ADDED:Exact the win position

--- Snakeladdersimulator.sh
+++ Snakeladdersimulator.sh
@@ -10,7 +10,7 @@ LADDER=1
 SNAKE=2
 
 playerPosition=0
-
+endPosition=10
 
 function getNumber() {
 	rno=$((RANDOM%6+1))
@@ -18,13 +18,37 @@ function getNumber() {
 }
 getNumber
 
+function getExactPosition() {
+
+	if [ $playerPosition -lt $START_POSITION ]
+	then
+		playerPosition=$START_POSITION
+	fi
+
+	if [ $playerPosition -gt $endPosition ]
+	then
+		playerPosition=$((playerPosition-$rno))
+	fi
+}
+
 function checkOption () {
+
+	while [[ $playerPosition -lt $endPosition ]]
+	do
 		choice=$((RANDOM%3))
-	case $choice in
-		$NO_PLAY) playerPosition=$(($playerPosition+0)) ;;
-		$LADDER)	playerPosition=$(( $playerPosition+$rno )) ;;
-		$SNAKE) playerPosition=$(( $playerPositon-$rno )) ;;
-	esac
+		case $choice in
+			$NO_PLAY) playerPosition=0 ;;
+			$LADDER) playerPosition=$(( $playerPosition+$rno )) ;;
+
+			$SNAKE) if [[ $playerposition -lt 0 ]]
+					  then
+					  		playerPosition=0
+				  	  else
+							playerPosition=$(( $playerPositon-$rno ))
+					  fi ;;
+		esac
+		getExactPosition
+	done
 		echo $playerPosition
 }
 checkOption
